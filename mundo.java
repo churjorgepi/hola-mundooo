4909b6a054f686b0af271b145c99a100142156ea
Maria Jose Lugo <165929364+Mariajoselugo@users.noreply.github.com>
2024-08-28 13:46:00 -0500

    Add files via upload

---
+++ mundo.java
@@ -0,0 +1,9 @@
+public class mundo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// SUBO LA CLASE A GITHUB.
+		System.out.println("¡Hola, Mundo!");
+		//Modificado por María José
+		System.out.println("¡Soy María José");
+	}
+}
